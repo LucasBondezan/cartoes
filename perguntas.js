cdbceecef5100ce9c349e82024bd25ac3d24f07a
LucasBondezan <bondezan.lucasv@gmail.com>
2024-11-19 09:25:23 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Inglaterra?',
+    'A capital da Inglaterra é Londres'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma exceção?',
+    'Situações inválidas que ocorrem durante o processamento e impedem que o programa continue seu fluxo normal até que a situação seja de algum modo resolvida.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Qual a tradução da seguinte frase: One bad chapter doesn’t mean your story is over.?',
+    'Um capítulo ruim não significa que a sua história acabou'
+)
